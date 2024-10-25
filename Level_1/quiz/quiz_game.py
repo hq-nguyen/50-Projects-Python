b3fe96d7375ba0b41b668d759f85f3cbe3982fc8
Hai-Quan Nguyen <136264412+hq-nguyen@users.noreply.github.com>
2024-10-25 15:46:01 +0000

    raw

---
+++ Level_1/quiz/quiz_game.py
@@ -0,0 +1,20 @@
+# quiz.py
+
+import random
+from string import ascii_lowercase
+
+NUM_QUESTIONS_PER_QUIZ = 5
+QUESTIONS = {
+    "What's one effect of calling random.seed(42)": [
+        "The random numbers are reproducible.",
+        "The random numbers are more random.",
+        "The computer clock is reset.",
+        "The first random number is always 42.",
+    ],
+    "When does __name__ == '__main__' equal True in a Python file": [
+        "When the file is run as a script",
+        "When the file is imported as a module",
+        "When the file has a valid name",
+        "When the file only has one function",
+    ]
+}
